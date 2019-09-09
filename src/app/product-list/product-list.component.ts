9e410abfd7b2fb6d97401af8559cd094c2e54dcc
Nirav Kakkad <nirav.kakkad@niiconsulting.com>
2019-09-09 18:05:20 +0530

    Initail commit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {products} from "../products";
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products = products;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  share() {
+    console.log("You shared the item");
+ }
+ onNotify() {
+  alert('You will be notified when the product goes on sale');
+}
+}
