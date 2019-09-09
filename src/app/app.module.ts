a47ee377a7bb1271d768d3f41f1cd0ea7da3ff34
Nirav Kakkad <nirav.kakkad@niiconsulting.com>
2019-09-09 18:25:24 +0530

    Added Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertComponent } from './product-alert/product-alert.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     TopBarComponent,
-    ProductAlertComponent
+    ProductAlertComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'', component: ProductListComponent},
+      {path: 'products/:productId', component: ProductDetailsComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
