2fc6a54656b61a59e95a0c27027761aab81e2545
Nirav Kakkad <nirav.kakkad@niiconsulting.com>
2019-09-10 10:11:45 +0530

    routing added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  product;
+  
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')];
+      console.log(this.product);
+      
+    });
   }
 
+  constructor(private route : ActivatedRoute) { }
 }
